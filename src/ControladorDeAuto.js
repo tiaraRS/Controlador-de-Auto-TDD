b05fd4020b70b297622557bac1fd0ee517997cfb
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-10-09 00:49:34 -0200

    F1 completa : avanzar una posicion en superficie y posicion inicial por defecto

--- src/ControladorDeAuto.js
+++ src/ControladorDeAuto.js
@@ -2,7 +2,7 @@ function controlarAuto(cadenaDeControlAuto) {
   let x = 4;
   let y = 0;
   let orientacion = "N";
-  if(cadenaDeControlAuto=="A") y++;
+  if(cadenaDeControlAuto[0]=="A") y++;
   let posicionFinal = `(${x},${y})${orientacion}`;
   return posicionFinal;
 }
--- src/ControladorDeAuto.test.js
+++ src/ControladorDeAuto.test.js
@@ -5,4 +5,16 @@ describe("controlarAuto", () => {
   it("deberia avanzar una posición del auto con superficie y posicion inicial por defecto", () => {
     expect(controlarAuto("A")).toEqual("(4,1)N");
   });
+  it("deberia quedarse en la misma posicion para comando B", () => {
+    expect(controlarAuto("B")).toEqual("(4,0)N");
+  });
+  it("deberia quedarse en la misma posicion para comando ''", () => {
+    expect(controlarAuto("")).toEqual("(4,0)N");
+  });
+  it("deberia avanzar una posicion con el comando 'ABAAA'", () => {
+    expect(controlarAuto("ABAAA")).toEqual("(4,1)N");
+  });
+  it("deberia avanzar una posicion con el comando 'ABASDFAAA'", () => {
+    expect(controlarAuto("ABASDFAAA")).toEqual("(4,1)N");
+  });
 });
